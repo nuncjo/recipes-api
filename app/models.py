ef55f578d59f0a21928a9d67bd55735c5beb8ec3
nuncjo <rafal.sobczynski@gmail.com>
2026-03-17 18:35:28 +0100

    for review

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -9,3 +10,16 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class RecipeRating(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ratings")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stars = models.PositiveSmallIntegerField(
+        choices=[(i, f"{i} star{'s' if i > 1 else ''}") for i in range(1, 6)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("recipe", "user")
+        ordering = ["-created_at"]
